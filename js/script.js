aacfd7542f93583be33e68775a2a02b978bd32a1
zGiusee <giusebelforte@gmail.com>
2024-01-29 14:26:01 +0100

    [UPDATE] Mostrate a video le task usando server.php e una chiamata api

--- js/script.js
+++ js/script.js
@@ -4,10 +4,19 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            myApiUrl: 'server.php',
+            tasksArray: [],
         }
     },
+    mounted() {
+        this.getTasksArray()
+    },
     methods: {
-
+        getTasksArray() {
+            axios.get(this.myApiUrl).then((response) => {
+                this.tasksArray = response.data;
+                console.log(this.tasksArray);
+            })
+        }
     },
 }).mount('#app')
--- server.php
+++ server.php
@@ -1,1 +1,8 @@
 <?php
+
+// Recupero il contenuto di 'tasks.json'
+$tasksArray = file_get_contents('tasks.json');
+
+header('Content-Type: application/json');
+
+echo $tasksArray;
