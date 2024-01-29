00c7601c0fd4fe97329d1a5ef57ea305cd2e0dd2
zGiusee <giusebelforte@gmail.com>
2024-01-29 14:50:18 +0100

    [UPDATE] Bonus n1 e bonus n2 fatti

--- js/script.js
+++ js/script.js
@@ -9,14 +9,20 @@ createApp({
         }
     },
     mounted() {
+        // Richiamo del funzione che recupera l'array dall'api
         this.getTasksArray()
     },
     methods: {
+        // Chiamata axios per recuperare i dati
         getTasksArray() {
             axios.get(this.myApiUrl).then((response) => {
                 this.tasksArray = response.data;
                 console.log(this.tasksArray);
             })
+        },
+        // Definisco la funzione di sbarramento delle task
+        doneToggle(index) {
+            this.tasksArray[index].done = !this.tasksArray[index].done
         }
     },
 }).mount('#app')
--- server.php
+++ server.php
@@ -3,6 +3,8 @@
 // Recupero il contenuto di 'tasks.json'
 $tasksArray = file_get_contents('tasks.json');
 
+// Applico la funzione header per la lettura del contenuto
 header('Content-Type: application/json');
 
+// Invio i dati in forma json
 echo $tasksArray;
